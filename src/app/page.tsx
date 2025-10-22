6e1b705a654c4b5811c19867dd0cb8b19bf268ee
Anubhav Sharma <anubhav@Anubhavs-MacBook-Air.local>
2025-10-23 01:25:05 +0530

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#0a0a0a] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://ezylife-in.vercel.app"
+            href="https://fin-best.vercel.app"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -42,7 +42,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Check Live Changes.
+            Check Live Changes 
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
